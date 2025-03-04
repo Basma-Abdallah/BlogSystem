0e5fdd1df004dc9a8d780a8ea94c3be3c53a2b22
Basma Abdallah <engbasmamohamad@gmail.com>
2025-03-04 19:33:43 +0200

    seconed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,17 +5,21 @@
 </head>
 <body>
     <h1>Blog Posts</h1>
+    @if(Auth::check())
     <a href="{{ route('posts.create') }}">Create New Post</a>
+    @endif
     <ul>
         @foreach ($posts as $post)
             <li>
                 <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
+                @if(Auth::check())
                 <a href="{{ route('posts.edit', $post) }}">Edit</a>
                 <form action="{{ route('posts.destroy', $post) }}" method="POST" style="display:inline;">
                     @csrf
                     @method('DELETE')
                     <button type="submit">Delete</button>
                 </form>
+                @endif
             </li>
         @endforeach
     </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+
+Auth::routes();
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('posts', PostController::class)->except(['index', 'show']);
+});
 Route::get('/', [PostController::class, 'index'])->name('index');
 
 Route::resource('posts', PostController::class);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
